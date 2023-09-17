0686dfa44bc6bf26810e744f9951ac7aa418a95e
B7Bprog <belabertalan@gmail.com>
2023-09-17 22:43:20 +0100

    complete accessing-arrays.js

--- accessing-arrays/accessing-arrays.js
+++ accessing-arrays/accessing-arrays.js
@@ -1,7 +1,42 @@
-const colors = ["red", "green", "blue", "yellow"];
+const colours = ["red", "green", "blue", "yellow"];
 
-const firstColor = colors[0];
-const nonExistentColor = colors[99];
+const firstColour = _;
+const nonExistentColour = _;
 
-console.log(firstColor); // 'red'
-console.log(nonExistentColor); // undefined
+console.log(firstColour); // 'red'
+console.log(nonExistentColour); // undefined
+
+const selectedIndex = 2;
+const fruits = ["apple", "grape", "orange", "mango", "kiwi"];
+
+const chosenFruit = _;
+console.log(chosenFruit); // 'orange'
+
+const dinnerMenu = [
+  ["salad", "crackers"],
+  ["steak", "potatoes"],
+  ["ice cream", "chocolate sauce"],
+];
+
+const mainCourse = _;
+console.log(mainCourse); // 'steak'
+
+const people = [
+  { name: "Alice", favoriteColour: "blue" },
+  { name: "Bob", favoriteColour: "red" },
+  { name: "Evie", favoriteColour: "green" },
+];
+
+const eviesFavoriteColour = _;
+console.log(eviesFavoriteColour); // 'green'
+
+////// Would this work?
+
+const team = [
+  { name: "Alice", favColour: "blue" },
+  { name: "Bob", favColour: "red" },
+  { name: "Grace", favColour: "green" },
+];
+
+const gracesFavColour = team.favColour;
+//const gracesFavColour = team.favColour[3];
